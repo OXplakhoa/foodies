bc1a1db38d46a9ce0f7ba2d1fbcfd4ce64ab3e0b
OXplakhoa <accbangbangchuot@gmail.com>
2025-04-07 15:15:32 +0700

    Add Image SlideShow

---
+++ components/images/image-slideshow.js
@@ -0,0 +1,46 @@
+'use client';
+import { useEffect, useState } from "react";
+
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
+import classes from "./image-slideshow.module.css";
+import Image from "next/image";
+
+const images = [
+  { image: burgerImg, alt: "A delicious, juicy burger" },
+  { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
+  { image: pizzaImg, alt: "A delicious pizza" },
+  { image: schnitzelImg, alt: "A delicious schnitzel" },
+  { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
+
+export default function ImageSlideShow() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIdx) =>
+        prevIdx < images.length - 1 ? prevIdx + 1 : 0
+      );
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+  return (
+    <div className={classes.slideshow}>
+      {images.map((img, idx) => (
+        <Image
+          key={idx}
+          src={img.image}
+          className={idx === currentImageIndex ? classes.active : ""}
+          alt={img.alt}
+        />
+      ))}
+    </div>
+  );
+}
