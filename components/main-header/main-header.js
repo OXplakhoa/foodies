817854915f770a148143eadcdc3afe98d02c2e16
OXplakhoa <accbangbangchuot@gmail.com>
2025-04-07 14:50:26 +0700

    Add stying HomePage

---
+++ components/main-header/main-header.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-bg";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image priority src={logoImg} alt="A plate with food on it" />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browser Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodie Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
