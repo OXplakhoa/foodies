8d60fa0cc7d89f876d0fd0e0c9bc69a7891c3ebc
OXplakhoa <accbangbangchuot@gmail.com>
2025-04-09 12:23:39 +0700

    Fetching Meals & add turbopack

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,25 +1,27 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+  const meals = await getMeals();
+  console.log(meals);
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created <span className={classes.highlight}>by you</span>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun
         </p>
-        <p className={classes.cta}> 
-            <Link href="/meals/share">
-              Share your favorite recipe
-            </Link>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
