85e9af38af78dff4df100706804ce76b8682b36a
OXplakhoa <accbangbangchuot@gmail.com>
2025-04-18 18:51:11 +0700

    Add dynamic metadata & config the nextjs cache

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -4,9 +4,22 @@ import Link from "next/link";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetail({ params }) {
   const meal = getMeal(params.mealSlug);
-  if (!meal){
+  if (!meal) {
     notFound();
   }
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
